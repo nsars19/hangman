84149f5870853db05bf6066e39da41551385f6b9
Nick <nsardonini@gmail.com>
2020-04-18 22:43:33 -0400

    add functionality for #load_game

--- hangman.rb
+++ hangman.rb
@@ -1,31 +1,47 @@
 # TODO
 # -Serialization
-# --save option at end of turn
 # --load option at beginning of game
 require 'json'
 
 class Board
+  attr_writer :word, :guesses_left, :letters_guessed, :board
+
   # Remove newline & carriage return chars. from each word
   @@word_list = File.open('words.txt', 'r').map do |word|
     word = word[0..-3]
   end.filter { |word| word.size >= 5 && word.size <= 12 }
 
-  def initialize
+  def initialize loaded_game = false
+    @loaded_game     = loaded_game
     @word            = get_random_word
     @guesses_left    = 6
     @letters_guessed = []
     @board           = []
-    create_board
+    create_board    if @board.empty?
+    game_start  unless @loaded_game
+  end
+  
+  private
+  def play_saved_game word, guesses_left, letters_guessed, board
+    @letters_guessed = letters_guessed
+    @guesses_left    = guesses_left
+    @board           = board
+    @word            = word
+    
+    display_board
+    until game_over?
+      make_guess gets.strip
+      display_board
+    end
   end
 
   def game_start
     puts "Type 'play' to start a new game, or type 'load' to load a saved game."
     start_choice = gets.chomp
-    show_saved_games if start_choice == 'load'
+    choose_save_game if start_choice == 'load'
     play_game        if start_choice == 'play'
   end
   
-  private
   def play_game
     puts "Type a letter to start playing hangman! or type 'save' to save and exit mid-game."
     until game_over?
@@ -35,11 +51,22 @@ class Board
     puts "#{@word} was the secret word."
   end
 
-  def show_saved_games
-    puts Dir.entries "./saves"
-
+  def choose_save_game
+    saves = Dir.entries("./saves").filter {|e| e[-4..-1] == 'json' }
+    saves.each { |e| puts e }
+    puts "Select a save file to load. Type the first six digits to load a file."
+    
+    file_choice = gets.chomp
+    saves.each { |e| load_game(e) if file_choice == e[0..5] }
   end
 
+  def load_game file
+    saved_game = File.open "./saves/#{file}"
+    
+    save = JSON.load(saved_game)
+    play_saved_game save['word'], save['guesses_left'], save['letters_guessed'], save['board']
+  end
+  
   def save_and_exit
     Dir.mkdir('saves') unless Dir.exists?('saves')
 
@@ -50,7 +77,7 @@ class Board
       :letters_guessed => @letters_guessed
     }
 
-    time = Time.new.strftime("%m%d%Y_%k%M%S")
+    time = Time.new.strftime("%k%M%S_%m%d%Y")
     filename = "./saves/#{time}.json"
 
     File.open(filename, 'w') do |file|
@@ -59,11 +86,13 @@ class Board
 
     exit
   end
+
   def make_guess guess
     save_and_exit if guess == 'save'
     until !@letters_guessed.include?(guess) && guess.length == 1
       puts "Please select a single letter that you have not already chosen:"
       guess = gets.chomp
+      save_and_exit if guess == 'save'
     end
 
     @letters_guessed << guess
@@ -108,4 +137,4 @@ class Board
   end
 end
 
-Board.new.game_start
+Board.new
